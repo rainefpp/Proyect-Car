6e82fdd66ba79b73852d40c8dcebe0f46862daf0
francop <fparedesp@ing.ucsc.cl>
2025-05-26 21:27:03 -0400

    version mobile de section coments
    quedo lista la version mobile, faltan tablet y desktop.

--- resources/views/livewire/components/coments-home.blade.php
+++ resources/views/livewire/components/coments-home.blade.php
@@ -2,20 +2,24 @@
     <h2 class="titulo-coments">¿Por qué confiar en Carzone?</h2>
     <h6 class="subtitulo-coments">Descubre lo que nuestros clientes dicen sobre la calidad de nuestros autos</h6>
     <div class="card-coment">
-        <img class="img-card-coment">
+        <img class="img-card-coment" src="images/banners/xavier.png" alt="Pefil" width="100" height="100">
         <h3 class="titulo-card-coment">– Javier R.</h3>
         <p class="descrption-card-coment">"Comprar en Carzone fue facilísimo. Me entregaron el auto rápidamente y la
             atención al cliente fue de primera. ¡Totalmente recomendable!"</p>
     </div>
     <div class="card-coment">
-        <img class="img-card-coment">
+        <img class="img-card-coment" src="images/banners/kmi.png" alt="Pefil" width="100" height="100">
         <h3 class="titulo-card-coment">– Camila G.</h3>
         <p class="descrption-card-coment">"Encontré justo el auto que quería y a un gran precio. Todo fue transparente y
             fácil. ¡Definitivamente volvería a comprar en Carzone!"</p>
     </div>
-    <h2 class="titulo-coments">Tu camino hacia el auto ideal empieza aquí</h2>
-    <h6 class="subtitulo-coments">En Carzone, simplificamos la búsqueda y compra de vehículos para que encuentres el
-        auto que necesitas de forma rápida y confiable. Con una amplia variedad de opciones y un servicio enfocado en la
-        satisfacción del cliente</h6>
-    <img class="img-coments">
+    <div class="grid gap-8" style="padding: 16px;">
+        <h2 class="titulo-coments" style="text-align: start;">Tu camino hacia el auto ideal empieza aquí</h2>
+        <h6 class="subtitulo-coments" style="justify-self: start;width:auto;">En Carzone, simplificamos la búsqueda y
+            compra de vehículos para que encuentres el
+            auto que necesitas de forma rápida y confiable. Con una amplia variedad de opciones y un servicio enfocado
+            en la
+            satisfacción del cliente</h6>
+        <img class="img-coments" src="images/banners/car-coments.png" alt="Pefil">
+    </div>
 </div>
