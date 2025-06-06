89297028d86596626f2413a90fe27b9cec98602d
francop <fparedesp@ing.ucsc.cl>
2025-06-05 20:15:03 -0400

    section coments en home
    se termino la version de tablet, falta solo la desktop.

--- resources/views/livewire/components/coments-home.blade.php
+++ resources/views/livewire/components/coments-home.blade.php
@@ -1,25 +1,33 @@
 <div class="contenedor-coments">
     <h2 class="titulo-coments">¿Por qué confiar en Carzone?</h2>
     <h6 class="subtitulo-coments">Descubre lo que nuestros clientes dicen sobre la calidad de nuestros autos</h6>
-    <div class="card-coment">
-        <img class="img-card-coment" src="images/banners/xavier.png" alt="Pefil" width="100" height="100">
-        <h3 class="titulo-card-coment">– Javier R.</h3>
-        <p class="descrption-card-coment">"Comprar en Carzone fue facilísimo. Me entregaron el auto rápidamente y la
-            atención al cliente fue de primera. ¡Totalmente recomendable!"</p>
-    </div>
-    <div class="card-coment">
-        <img class="img-card-coment" src="images/banners/kmi.png" alt="Pefil" width="100" height="100">
-        <h3 class="titulo-card-coment">– Camila G.</h3>
-        <p class="descrption-card-coment">"Encontré justo el auto que quería y a un gran precio. Todo fue transparente y
-            fácil. ¡Definitivamente volvería a comprar en Carzone!"</p>
-    </div>
-    <div class="grid gap-8" style="padding: 16px;">
-        <h2 class="titulo-coments" style="text-align: start;">Tu camino hacia el auto ideal empieza aquí</h2>
-        <h6 class="subtitulo-coments" style="justify-self: start;width:auto;">En Carzone, simplificamos la búsqueda y
-            compra de vehículos para que encuentres el
-            auto que necesitas de forma rápida y confiable. Con una amplia variedad de opciones y un servicio enfocado
-            en la
-            satisfacción del cliente</h6>
-        <img class="img-coments" src="images/banners/car-coments.png" alt="Pefil">
+    <div class="grid md:grid-cols-2 ">
+        <div class="card-coment">
+            <img class="img-card-coment" src="images/banners/xavier.png" alt="Pefil" width="100" height="100">
+            <h3 class="titulo-card-coment">– Javier R.</h3>
+            <p class="descrption-card-coment">"Comprar en Carzone fue facilísimo. Me entregaron el auto rápidamente y la
+                atención al cliente fue de primera. ¡Totalmente recomendable!"</p>
+        </div>
+        <div class="card-coment">
+            <img class="img-card-coment" src="images/banners/kmi.png" alt="Pefil" width="100" height="100">
+            <h3 class="titulo-card-coment">– Camila G.</h3>
+            <p class="descrption-card-coment">"Encontré justo el auto que quería y a un gran precio. Todo fue
+                transparente y
+                fácil. ¡Definitivamente volvería a comprar en Carzone!"</p>
+        </div>
+        <div class="grid gap-8 subcont-coments mt-8" style="padding: 16px;">
+            <div class="grid mt-8">
+                <h2 class="titulo-coments" style="text-align: start;">Tu camino hacia el auto ideal empieza aquí</h2>
+                <h6 class="subtitulo-coments" style="justify-self: start;width:auto;">En Carzone, simplificamos la
+                    búsqueda
+                    y
+                    compra de vehículos para que encuentres el
+                    auto que necesitas de forma rápida y confiable. Con una amplia variedad de opciones y un servicio
+                    enfocado
+                    en la
+                    satisfacción del cliente</h6>
+            </div>
+            <img class="img-coments" src="images/banners/car-coments.png" alt="Pefil">
+        </div>
     </div>
 </div>
