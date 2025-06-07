dcbc35378b3d83a07ccbf47579a458d8cd9d1564
francop <fparedesp@ing.ucsc.cl>
2025-06-07 06:56:11 -0400

    version responsibe de section coments lista

--- resources/views/livewire/components/coments-home.blade.php
+++ resources/views/livewire/components/coments-home.blade.php
@@ -1,22 +1,22 @@
 <div class="contenedor-coments">
     <h2 class="titulo-coments">¿Por qué confiar en Carzone?</h2>
     <h6 class="subtitulo-coments">Descubre lo que nuestros clientes dicen sobre la calidad de nuestros autos</h6>
-    <div class="grid md:grid-cols-2 ">
-        <div class="card-coment">
+    <div class="grid md:grid-cols-2 cont-prim">
+        <div class="card-coment card-coment-izq">
             <img class="img-card-coment" src="images/banners/xavier.png" alt="Pefil" width="100" height="100">
             <h3 class="titulo-card-coment">– Javier R.</h3>
-            <p class="descrption-card-coment">"Comprar en Carzone fue facilísimo. Me entregaron el auto rápidamente y la
+            <p class="description-card-coment">"Comprar en Carzone fue facilísimo. Me entregaron el auto rápidamente y la
                 atención al cliente fue de primera. ¡Totalmente recomendable!"</p>
         </div>
-        <div class="card-coment">
+        <div class="card-coment card-coment-der">
             <img class="img-card-coment" src="images/banners/kmi.png" alt="Pefil" width="100" height="100">
             <h3 class="titulo-card-coment">– Camila G.</h3>
-            <p class="descrption-card-coment">"Encontré justo el auto que quería y a un gran precio. Todo fue
+            <p class="description-card-coment">"Encontré justo el auto que quería y a un gran precio. Todo fue
                 transparente y
                 fácil. ¡Definitivamente volvería a comprar en Carzone!"</p>
         </div>
-        <div class="grid gap-8 subcont-coments mt-8" style="padding: 16px;">
-            <div class="grid mt-8">
+        <div class="grid gap-8 subcont-coments" style="padding: 16px;">
+            <div class="grid gap-8 mt-8">
                 <h2 class="titulo-coments" style="text-align: start;">Tu camino hacia el auto ideal empieza aquí</h2>
                 <h6 class="subtitulo-coments" style="justify-self: start;width:auto;">En Carzone, simplificamos la
                     búsqueda
