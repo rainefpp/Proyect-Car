2e7eeaa791eae7a6d0c73d3ba7d9209db4cc4d75
francop <fparedesp@ing.ucsc.cl>
2025-06-20 17:18:44 -0400

    version mobile de quienes somos
    falta implementar lo responsive correspondiente a tablet y desktop.

--- resources/views/livewire/quiene-somos-page.blade.php
+++ resources/views/livewire/quiene-somos-page.blade.php
@@ -1,3 +1,47 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+    <div class="grid gap-4" style="background-color: #E7E8E8;">
+        <img src="images/banners/qs-intro.png" alt="people" width="100%" height="100%">
+        <h1 class="mt-2 text-center qs-titulo">Conocer nuestra historia es crecer juntos</h1>
+        <div class="grid gap-4" style="justify-items: center;">
+            <div class="card-qs">
+                <h1 class="card-qs-titulo">Nuestra Misión</h1>
+                <p class="card-qs-texto mt-4">Brindar una experiencia de compra automotriz fácil, segura y confiable,
+                    ofreciendo productos de calidad y atención personalizada, para que cada cliente encuentre justo lo
+                    que necesita para su vehículo.</p>
+            </div>
+            <div class="card-qs">
+                <h1 class="card-qs-titulo">Nuestra Visión</h1>
+                <p class="card-qs-texto mt-4">Ser la tienda online líder en productos automotrices, reconocida por
+                    nuestra innovación, servicio al cliente y compromiso con la excelencia.</p>
+            </div>
+            <div class="card-qs">
+                <h1 class="card-qs-titulo">Nuestra Historia</h1>
+                <p class="card-qs-texto mt-4">Carzone nació con la idea de revolucionar la forma en que las personas
+                    acceden a productos automotrices de calidad. Comenzamos como un pequeño emprendimiento apasionado
+                    por los autos, y con el tiempo, gracias a la confianza de nuestros clientes y el esfuerzo de nuestro
+                    equipo, nos consolidamos como una plataforma confiable, rápida y segura. Hoy, seguimos creciendo
+                    para acercar el mundo automotriz a cada rincón del país.</p>
+            </div>
+            <img src="images/banners/qs-outro.png" alt="gente" width="100%" height="100%">
+        </div>
+        <h1 class="qs-second-titulo mt-4">Nuestros Valores</h1>
+        <div class="grid gap-4 text-center" style="justify-items: center;margin-bottom:2rem;">
+            <div class="grid mt-4 gap-4">
+                <img src="images/compromiso-qs.png" alt="compromiso" width="130" height="130">
+                <h1 class="qs-second-subtitulo">Compromiso</h1>
+            </div>
+            <div class="grid mt-4 gap-4">
+                <img src="images/calidad-qs.png" alt="calidad" width="130" height="130">
+                <h1 class="qs-second-subtitulo">Calidad</h1>
+            </div>
+            <div class="grid mt-4 gap-4">
+                <img src="images/confianza-qs.png" alt="Confianza" width="130" height="130">
+                <h1 class="qs-second-subtitulo">Confianza</h1>
+            </div>
+            <div class="grid mt-4 gap-4">
+                <img src="images/pasion-qs.png" alt="Pasion" width="130" height="130">
+                <h1 class="qs-second-subtitulo">Pasión por los autos</h1>
+            </div>
+        </div>
+    </div>
 </div>
